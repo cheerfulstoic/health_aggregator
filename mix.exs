26d44b820804538e8320dbcd693b12c11a53dd45
Brian Underwood <public@brian-underwood.codes>
2021-11-23 21:21:54 +0100

    Use my own branch of contex while I figure out if my patch is good

--- mix.exs
+++ mix.exs
@@ -41,7 +41,7 @@ defmodule HealthAggregator.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 0.16.0"},
 
-      {:contex, path: "/Users/cheerfulstoic/github/mindok/contex"},
+      {:contex, git: "git@github.com:cheerfulstoic/contex.git"},
       {:floki, ">= 0.30.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.5"},
       {:esbuild, "~> 0.2", runtime: Mix.env() == :dev},
